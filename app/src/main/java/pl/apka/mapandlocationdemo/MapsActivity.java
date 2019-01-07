0ff419971ff1bb0f6a37ba7d955651a95d896ec0
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-01-07 22:16:45 +0100

    locality, thoroughfare and admin added

--- app/src/main/java/pl/apka/mapandlocationdemo/MapsActivity.java
+++ app/src/main/java/pl/apka/mapandlocationdemo/MapsActivity.java
@@ -90,7 +90,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
 
                     if (listAddresses != null && listAddresses.size() > 0) {
-                        Log.i("Place info", listAddresses.get(0).toString());
+                        String address = "";
+
+                        if (listAddresses.get(0).getAdminArea() != null) {
+                            address += listAddresses.get(0).getAdminArea() + " ";
+
+                        }
+
+                        if (listAddresses.get(0).getLocality() != null) {
+                            address += listAddresses.get(0).getLocality() + " ";
+
+                        }
+
+                        if (listAddresses.get(0).getThoroughfare() != null) {
+                            address += listAddresses.get(0).getThoroughfare() + " ";
+
+                        }
+
                     }
 
                 } catch (Exception e) {
